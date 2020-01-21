750aea9afed36e35228be065f1f10501d3a4ff27
chaitu01 <chaitanyach31@gmail.com>
2020-01-21 18:22:14 +1100

    Add files via upload

---
+++ capatone.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# this notebook is used to complete capstone project of ibm data science certificate
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
